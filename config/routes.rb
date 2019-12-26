73f95c899030035abce7db285befaf8dc262e59f
iinotomoya <iinotomoya@yahoo.co.jp>
2019-12-26 19:53:34 +0900

    Itemデータ取得

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "items#index"
-  resources :items, only: [:index, :new, :edit, :update]
+  root to: "items#show"
+  resources :items, only: [:index, :new, :edit, :update, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
