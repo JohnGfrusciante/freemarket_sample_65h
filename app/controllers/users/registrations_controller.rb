fa49e11262004ead6c21d427894ecdf33fe4bf59
MasanoriNishizawa <57927373+MasanriNishizawa@users.noreply.github.com>
2019-12-29 17:53:32 +0900

    sns修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,7 +13,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
     # super
   end
 
-def password = Devise.friendly_token.first(7)
+def create
+  password = Devise.friendly_token.first(7)
   if session[:provider].present? && session[:uid].present?
     @user = User.new(nickname:session[:nickname], email: session[:email], password: "password", password_confirmation: "password", f_name_kana: session[:f_name_kana],l_name_kana: session[:l_name_kana], f_name_kanji: session[:f_name_kanji], l_name_kanji: session[:l_name_kanji], birthday: session[:birthday], tel: params[:user][:tel])
     sns = SnsCredential.new(user_id: @user.id,uid: session[:uid], provider: session[:provider])
