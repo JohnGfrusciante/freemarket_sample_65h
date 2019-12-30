fd64a0376a7a30baf8ae4392b11f002fe5427183
ken-suyama <kh_6bbl@i.softbank.jp>
2019-12-30 17:29:12 +0900

    コメントアウトの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,15 +12,6 @@ class ItemsController < ApplicationController
     
     @item = Item.new(item_params)
     @item.save
-
-  #   if @item.save
-  #     params[:item_images]['filename'].each do |a|
-  #       @item_image = @item.item_images.create!(filename: a)
-  #     end
-  #     redirect_to root_path, notice: '出品しました。'
-  #   else
-  #     render :new
-  #   end
    end
 
   private
