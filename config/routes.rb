4f296a99192556183a006121acb47ae7feec2eae
iinotomoya <iinotomoya@yahoo.co.jp>
2019-12-30 21:11:05 +0900

    トークンの取得

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     omniauth_callbacks: 'users/omniauth_callbacks'
   }  
-  root to: "items#index"
-  resources :items, only: [:index, :new, :edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "users#register_credit.html"
+  resources :items, only: [:index, :new, :edit, :update] do
+  end
+
+  post '/pay' => "items#index"
 end
