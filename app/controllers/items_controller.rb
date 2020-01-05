66b0c5b51d00f3165a4d0749b10d463da5b3758a
ken-suyama <kh_6bbl@i.softbank.jp>
2020-01-05 12:43:33 +0900

    コントローラーの記述変更
    ## What
    冗長だった部分をリファクタリング
    エラーハンドリングの追加
    
    ## Why
    繰り返し使う記述をメソッド化
    エラー画面を表示する為
    
    ## Etc
    destroyに関してはまだ機能がないので無視してください

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+
+  before_action :set_item, only: [:edit, :update, :destroy]
   def index
     @items= Item.all
   end
@@ -14,16 +16,22 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    if @item.present?
+      render :edit
+    else
+      redirect_to root_path, notice: '商品の情報がありません'
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to root_path, notice: '商品情報を更新しました'
+    else
+      render :edit, notice: '商品の更新に失敗しました'
+    end
   end
 
   def destroy
-    item = Item.find(params[:id])
     item.destroy
   end
 
@@ -31,5 +39,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :discription, :price, :condition, :shipping_charge, :shipping_date, :prefecture, item_images_attributes: [:id, :image, :_destroy])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
