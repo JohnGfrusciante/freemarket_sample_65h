147518d01ff105a4492b6f0d582a9e2ce03d334d
iinotomoya <iinotomoya@yahoo.co.jp>
2020-01-06 15:59:15 +0900

    商品詳細ページのサーバーサイド２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,8 @@ class ItemsController < ApplicationController
   # 参照する１つの商品情報を取得。選択したアイテムのIDを引数にする
     @item= Item.find(1)
   # item_idの値が同じレコードを取得
-    @image= ItemImage.where(item_id: @item.id)
-    gon.image= ItemImage.where(item_id: @item.id)
-
+    @image= @item.item_images.where(item_id: @item.id)
+    gon.image= @item.item_images.where(item_id: @item.id)
   # 出品者情報を取得（名前だけが欲しい）。seller_idとusersテーブルのidを紐付け
     @seller_name= User.find(@item.seller_id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }  
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :edit, :update, :destroy]  do
+  resources :items, only: [:index, :show, :new, :create, :edit, :update, :destroy]  do
     resources :item_images, only: [:create]
   end
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
