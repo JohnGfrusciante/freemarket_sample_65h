0cba9c1e3b5bba50d2b3028fc924e8c9c5cdf9f4
MasanoriNishizawa <57927373+MasanriNishizawa@users.noreply.github.com>
2020-01-07 20:24:54 +0900

    マイページview修正

--- app/assets/javascripts/mypages.js
+++ app/assets/javascripts/mypages.js
@@ -1,6 +1,15 @@
+var active1 = 'is-active1'
+var show1 = 'is-show1'
+var tab1 = 'tab1'
+var panel1 = 'panel1'
+var active2 = 'is-active2'
+var show2 = 'is-show2'
+var tab2 = 'tab2'
+var panel2 = 'panel2'
+
 document.addEventListener('DOMContentLoaded', function(){
 	// タブに対してクリックイベントを適用
-	const tabs = document.getElementsByClassName('tab1');
+	const tabs = document.getElementsByClassName(tab1);
 	for(let i = 0; i < tabs.length; i++) {
 		tabs[i].addEventListener('click', tabSwitch);
 	}
@@ -8,19 +17,19 @@ document.addEventListener('DOMContentLoaded', function(){
 	// タブをクリックすると実行する関数
 	function tabSwitch(){
 		// タブのclassの値を変更
-		document.getElementsByClassName('is-active1')[0].classList.remove('is-active1');
-		this.classList.add('is-active1');
+		document.getElementsByClassName(active1)[0].classList.remove(active1);
+		this.classList.add(active1);
 		// コンテンツのclassの値を変更
-		document.getElementsByClassName('is-show1')[0].classList.remove('is-show1');
+		document.getElementsByClassName(show1)[0].classList.remove(show1);
 		const arrayTabs = Array.prototype.slice.call(tabs);
 		const index = arrayTabs.indexOf(this);
-		document.getElementsByClassName('panel1')[index].classList.add('is-show1');
+		document.getElementsByClassName(panel1)[index].classList.add(show1);
 	};
 });
 
 document.addEventListener('DOMContentLoaded', function(){
 	// タブに対してクリックイベントを適用
-	const tabs = document.getElementsByClassName('tab2');
+	const tabs = document.getElementsByClassName(tab2);
 	for(let i = 0; i < tabs.length; i++) {
 		tabs[i].addEventListener('click', tabSwitch);
 	}
@@ -28,12 +37,12 @@ document.addEventListener('DOMContentLoaded', function(){
 	// タブをクリックすると実行する関数
 	function tabSwitch(){
 		// タブのclassの値を変更
-		document.getElementsByClassName('is-active2')[0].classList.remove('is-active2');
-		this.classList.add('is-active2');
+		document.getElementsByClassName(active2)[0].classList.remove(active2);
+		this.classList.add(active2);
 		// コンテンツのclassの値を変更
-		document.getElementsByClassName('is-show2')[0].classList.remove('is-show2');
+		document.getElementsByClassName(show2)[0].classList.remove(show2);
 		const arrayTabs = Array.prototype.slice.call(tabs);
 		const index = arrayTabs.indexOf(this);
-		document.getElementsByClassName('panel2')[index].classList.add('is-show2');
+		document.getElementsByClassName(panel2)[index].classList.add(show2);
 	};
 });
