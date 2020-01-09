5a58922ba1cd8038cc75a849cd5d71d9f912e1c0
MasanoriNishizawa <57927373+MasanriNishizawa@users.noreply.github.com>
2020-01-09 12:39:33 +0900

    マイページview修正

--- app/assets/javascripts/mypages.js
+++ app/assets/javascripts/mypages.js
@@ -1,48 +1,19 @@
-var active1 = 'is-active1'
-var show1 = 'is-show1'
-var tab1 = 'tab1'
-var panel1 = 'panel1'
-var active2 = 'is-active2'
-var show2 = 'is-show2'
-var tab2 = 'tab2'
-var panel2 = 'panel2'
-
-document.addEventListener('DOMContentLoaded', function(){
-	// タブに対してクリックイベントを適用
-	const tabs = document.getElementsByClassName(tab1);
-	for(let i = 0; i < tabs.length; i++) {
-		tabs[i].addEventListener('click', tabSwitch);
-	}
-
-	// タブをクリックすると実行する関数
-	function tabSwitch(){
-		// タブのclassの値を変更
-		document.getElementsByClassName(active1)[0].classList.remove(active1);
-		this.classList.add(active1);
-		// コンテンツのclassの値を変更
-		document.getElementsByClassName(show1)[0].classList.remove(show1);
-		const arrayTabs = Array.prototype.slice.call(tabs);
-		const index = arrayTabs.indexOf(this);
-		document.getElementsByClassName(panel1)[index].classList.add(show1);
-	};
+$(function($){
+	$('.tab1').click(function(){
+		$('.is-active1').removeClass('is-active1');
+		$(this).addClass('is-active1');
+		$('.is-show1').removeClass('is-show1');
+		const index = $(this).index();
+		$('.panel1').eq(index).addClass('is-show1');
+	});
 });
 
-document.addEventListener('DOMContentLoaded', function(){
-	// タブに対してクリックイベントを適用
-	const tabs = document.getElementsByClassName(tab2);
-	for(let i = 0; i < tabs.length; i++) {
-		tabs[i].addEventListener('click', tabSwitch);
-	}
-
-	// タブをクリックすると実行する関数
-	function tabSwitch(){
-		// タブのclassの値を変更
-		document.getElementsByClassName(active2)[0].classList.remove(active2);
-		this.classList.add(active2);
-		// コンテンツのclassの値を変更
-		document.getElementsByClassName(show2)[0].classList.remove(show2);
-		const arrayTabs = Array.prototype.slice.call(tabs);
-		const index = arrayTabs.indexOf(this);
-		document.getElementsByClassName(panel2)[index].classList.add(show2);
-	};
+$(function($){
+	$('.tab2').click(function(){
+		$('.is-active2').removeClass('is-active2');
+		$(this).addClass('is-active2');
+		$('.is-show2').removeClass('is-show2');
+		const index = $(this).index();
+		$('.panel2').eq(index).addClass('is-show2');
+	});
 });
