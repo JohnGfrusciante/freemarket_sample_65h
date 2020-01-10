114be23dbcb2faee2ed91bcd4d2cc89ad54d51fe
iinotomoya <iinotomoya@yahoo.co.jp>
2020-01-10 18:00:40 +0900

    修正２

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,10 +1,9 @@
 class CardController < ApplicationController
   require "payjp"
+  before_action :set_card, only: [:new, :delete, :show]
 
   def new
-    card = Card.where(user_id: 8)
-# card = Card.where(user_id: current_user.id)のちに変更
-    redirect_to action: "show" if card.exists?
+    redirect_to action: "show" if @card.presence
   end
 
   def pay #payjpとCardのデータベース作成を実施します。
@@ -13,13 +12,10 @@ class CardController < ApplicationController
       redirect_to action: "new"
     else
       customer = Payjp::Customer.create(
-#email: current_user.email, #なくてもOK
       card: params['payjp-token'],
       metadata: {user_id: 8}
-# metadata: {user_id: current_user.id}後に変更
-      ) #念の為metadataにuser_idを入れましたがなくてもOK
+      )
       @card = Card.new(user_id: 8, customer_id: customer.id, card_id: customer.default_card)
-# @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)後に変更
       if @card.save
         redirect_to action: "index", controller: "purchase"
       else
@@ -29,29 +25,27 @@ class CardController < ApplicationController
   end
 
   def delete #PayjpとCardデータベースを削除します
-    card = Card.find_by(user_id: 8)
-# card = Card.where(user_id: current_user.id).first後に変更
-    if card.blank?
+    if @card.blank?
     else
       Payjp.api_key = ENV["PAYJP_TEST_S_KEY"]
-# card.customer_id後に引数
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
-      card.delete
+      @card.delete
     end
     redirect_to action: "new"
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
-    card = Card.find_by(user_id: 8)
-# card = Card.where(user_id: current_user.id).first後に変更
-    if card.blank?
+    if @card.blank?
       redirect_to action: "new"
     else
       Payjp.api_key = ENV["PAYJP_TEST_S_KEY"]
-# card.customer_id後に引数
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
+  end
+
+  def set_card
+    @card = Card.find_by(user_id: 8)
   end
 end
--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,41 +1,35 @@
 class PurchaseController < ApplicationController
   require 'payjp'
+  before_action :set_card, only: [:index, :pay]
+
 
   def index
     @item = Item.find(2)
     @image = @item.item_images.where(item_id: @item.id)
-
-  # item_idの値が同じレコードを取得
-
-    card = Card.where(user_id: 8).first
-# card = Card.where(user_id: current_user.id).first
-
-
-    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
-    if card.blank?
-      #登録された情報がない場合にカード登録画面に移動
-      # redirect_to controller: "card", action: "new"
+    if @card.blank?
     else
       Payjp.api_key = ENV["PAYJP_TEST_S_KEY"]
-      #保管した顧客IDでpayjpから情報取得
-      customer = Payjp::Customer.retrieve(card.customer_id)
-
-      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
 
   def pay
-    @item = Item.find(2)
-    card = Card.where(user_id: 8).first
-# card = Card.where(user_id: current_user.id).first
+    if @card.presence
+      @item = Item.find(2)
+      Payjp.api_key = ENV['PAYJP_TEST_S_KEY']
+      Payjp::Charge.create(
+      :amount => @item.price,
+      :customer => @card.customer_id,
+      :currency => 'jpy',
+      )
+      render :done
+    else
 
-    Payjp.api_key = ENV['PAYJP_TEST_S_KEY']
-    Payjp::Charge.create(
-    :amount => @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
-    :customer => card.customer_id, #顧客ID
-    :currency => 'jpy', #日本円
-    )
-    render :done
+    end
+  end
+
+  def set_card
+    @card = Card.find_by(user_id: 8)
   end
 end
