252fb270a45288b222315295a98312633fcbbbfa
ken-suyama <kh_6bbl@i.softbank.jp>
2020-01-14 15:38:58 +0900

    パンくずリスト
    ## What
    パンくずが表示されるページにパンくずを設置
    
    ## Why
    自分がどのページにいるかを把握する為
    前のページに戻る為

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,33 @@
+crumb :root do
+  link "メルカリ", root_path
+end
+
+crumb :mypage do
+  link "マイページ", mypage_path(current_user.id)
+  parent :root
+end
+
+crumb :profiles do
+  link "プロフィール", profiles_path
+  parent :mypage
+end
+
+crumb :informations do
+  link "本人情報の登録", informations_path
+  parent :mypage
+end
+
+crumb :logout do
+  link "ログアウト", logout_path
+  parent :mypage
+end
+
+crumb :creditcard do
+  link "支払い方法", creditcard_path
+  parent :mypage
+end
+
+crumb :cardregister do
+  link "クレジットカード情報入力", cardregister_path
+  parent :creditcard
+end
